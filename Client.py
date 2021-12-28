50341fef7edd6266fe71d9a3cbe2b67d35c3a8bc
Bar Rozenman <barroz@post.bgu.ac.il>
2021-12-28 22:01:01 +0200

    update client

--- Client.py
+++ Client.py
@@ -55,4 +55,38 @@ def get_from_server(tcp):
         except:
             pass
     tcp.close()
-    
+
+def main():
+    print(style.GREEN + "Client started, listening for offer requests...")
+    while True:
+        #opening UDP socket for broadcast and TCP socket for game
+        UDP_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  
+        UDP_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        UDP_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        UDP_sock.bind(("", destination_port))
+        TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        TCP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        data, addr = UDP_sock.recvfrom(BYTES_TO_READ)
+        server_ip_address = str(addr[0])
+        UDP_sock.close()
+        #decode the data
+        try:
+            cookie, message_type, server_tcp_port = struct.unpack('LBH', data)  # get message and encode it as a the given format
+            if cookie == 0xabcddcba or message_type == 0x2:  # check if the message is as the expected format
+                print(style.GREEN + "Received offer from " + server_ip_address + " attempting to connect...")
+            TCP_socket.connect((server_ip_address, server_tcp_port))
+            nickname = input("Type In Your Nickname: ")
+            nickname = nickname.encode()
+            TCP_socket.sendall(nickname)
+            sys.stdout.write(style.YELLOW + TCP_socket.recv(BYTES_TO_READ).decode())  #get message of connecting to server and print it it to user
+            game = Thread(target=get_from_server, args=(TCP_socket,))
+            game.start() 
+            game.join()
+            print(style.RED + "Server disconnected, listening for offer requests...")
+        except:
+            ()
+
+            
+
+if __name__ == '__main__':
+    main()    
