9f959e42a5e5495fc84ea3087cdd96aa5e255b02
orishac <62713593+orishac@users.noreply.github.com>
2021-12-29 21:17:04 +0200

    port server

--- Server.py
+++ Server.py
@@ -22,7 +22,7 @@ class colors:
 
     
 #to avoid hard coded things, this is how we get the addresses
-port = 2028
+port = 2522
 ip_address = get_if_addr('eth1')
 #ip_address = "0.0.0.0"
 destination_port = 13117
